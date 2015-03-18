37a355bd37e1aa53ff590397c38a46aeca988c4d
totallyuneekname <student@HBW-STU-C17N327GG083.local>
2015-03-18 13:47:56 -0400

    added Javanese Wars and some other things
    see title.

---
+++ javawars.php
@@ -0,0 +1,4 @@
+<div class="section">
+    <h2>Javanese Wars</h2>
+    <p>In the 1800s, Java fought in a series of wars, most notably the Java War (1741-43) and the Invasion of Java, in 1811. The former was against the Dutch, who in 1741 led an attack on Java to try to gain control of the region. The war lasted for three years, and both sides suffered many losses.</p>
+</div>
