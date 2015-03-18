36b672e56176f5a7a6c0f8a9f9d55be2711030d4
totallyuneekname <student@HBW-STU-C17N327GG083.local>
2015-03-17 22:36:44 -0400

    some changes
    added content to Burma, changed font size, and linked to stylesheet

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <title>An overview of imperialism in Southeast Asia</title>
+        <link rel="stylesheet" type="text/css" href="main.css">
     </head>
     <body>
+        <font size="5">
         <?php
         include 'vod.php';
         echo "<hr>";
         include 'burma.php';
         echo "<hr>";
         ?>
+        </font>
     </body>
 </html>
