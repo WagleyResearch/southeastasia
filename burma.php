83ee9579042b5fe6528a57c4a8cafed54a10e9a7
totallyuneekname <student@HBW-STU-C17N327GG083.local>
2015-03-18 10:15:21 -0400

    Revert 23e7b3d..a7845f7
    This rolls back to commit 23e7b3dbcaf1c2801fc9199873f8af6ff292f609.

--- burma.php
+++ burma.php
@@ -2,7 +2,7 @@
 
     <div>
         <p>A &ldquo;series of monarchs&rdquo; tried to take control of Burma. In the 1800s, the country had become very unstable, and Britain took advantage of this by trying to take over the region. After three wars over 60 years, Britain finally took control of the country, and annexed it to be a part of British India. Britain then only favored some of the native groups in Burma, and introduced many of their own traditions and customs. This harshly weakened the Burmese culture.</p>
-        <img class="left" style="width: 15%;" src="http://www.aungsan.com/images/AungSan1gs.jpg">
+        <img class="right" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Location_Burma_%28Myanmar%29_ASEAN.svg/1200px-Location_Burma_%28Myanmar%29_ASEAN.svg.png" alt="Location of &nbsp;Burma&nbsp;&nbsp;(green)in ASEAN&nbsp;&nbsp;(dark grey)&nbsp; –&nbsp; [Legend]">
         <p>Aung San was a young law student in Burma, who in 1836 left his classes to be more involved with politics. He was against the British, and their imperialist mentality. He joined the Dobama Asiayone (Our Burma Union) as a general secretary, and helped to organize strikes across the country.</p>
-        <img class="right" style="width: 30%;" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Location_Burma_%28Myanmar%29_ASEAN.svg/1200px-Location_Burma_%28Myanmar%29_ASEAN.svg.png" alt="Location of &nbsp;Burma&nbsp;&nbsp;(green)in ASEAN&nbsp;&nbsp;(dark grey)&nbsp; –&nbsp; [Legend]">
     </div>
+Hello
