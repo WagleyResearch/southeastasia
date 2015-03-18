23e7b3dbcaf1c2801fc9199873f8af6ff292f609
totallyuneekname <student@HBW-STU-C17N327GG083.local>
2015-03-18 10:00:46 -0400

    rjn
    wekjnfkj

--- burma.php
+++ burma.php
@@ -5,3 +5,4 @@
         <img class="right" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Location_Burma_%28Myanmar%29_ASEAN.svg/1200px-Location_Burma_%28Myanmar%29_ASEAN.svg.png" alt="Location of &nbsp;Burma&nbsp;&nbsp;(green)in ASEAN&nbsp;&nbsp;(dark grey)&nbsp; –&nbsp; [Legend]">
         <p>Aung San was a young law student in Burma, who in 1836 left his classes to be more involved with politics. He was against the British, and their imperialist mentality. He joined the Dobama Asiayone (Our Burma Union) as a general secretary, and helped to organize strikes across the country.</p>
     </div>
+Hello
