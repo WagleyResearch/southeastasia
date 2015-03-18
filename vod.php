5d7a9bed9986c1f08a84fa0f939f777bf485e269
root <root@localhost.localdomain>
2015-03-18 13:51:20 -0400

    Edited VOC.php

--- vod.php
+++ vod.php
@@ -1,1 +1,11 @@
-Dutch East India Company (VOD)
+<div class="section">
+    <h2>Dutch East India Company</h2>
+    <p> A company that held a 21 year monopoly on trade in the East Indies, from the country of the Netherlands. It was the first company to offer stock.  Set records in that it sent millions of Europeans to work in Asia on 4,785 ships.  The company had government like powers, with the power to mint currency, convict and execute people, negotiate treaties and found colonies. The Dutch East India Company or Vereenigde Oostindische Compagnie (VOC) was formed in response to the English East India Company, as the Dutch saw that they would loose out with their companies,  if they were not granted similar powers. The first permanent trading post formed by the VOC was Banten, West Java.  This sparked the first of the Dutch spice trading posts.  The English East India Company attempted to establish trading ports in the east indies, however after ten english men were beheaded for conspiracy while attempting to make a deal with the Dutch about trading in the area, the brits pulled out most of their trading from the region. The VOC killed almost everyone on the Banda islands. </p>
+    <div class="image" style="float: left; size: 15%;">
+        <img src="">
+    </div>
+    <p></p>
+    <div class="image" style="float: right;">
+        <img style="" src="">
+    </div>
+</div>
