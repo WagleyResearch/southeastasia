688cdc17c99b62c5cea2584e548f50e5c7da45ed
totallyuneekname <student@HBW-STU-C17N327GG083.local>
2015-03-18 20:11:53 -0400

    Fixed margins and image sizes
    (For the most part)

--- burma.php
+++ burma.php
@@ -1,7 +1,7 @@
 <div class="section">
     <h2>Burma</h2>
     <div class="image">
-        <img src="http://www.aungsan.com/images/AungSan1gs.jpg">
+        <img width="20%" src="http://www.aungsan.com/images/AungSan1gs.jpg">
     </div>
     <p>A &ldquo;series of monarchs&rdquo; tried to take control of Burma. In the 1800s, the country had become very unstable, and Britain took advantage of this by trying to take over the region. After three wars over 60 years, Britain finally took control of the country, and annexed it to be a part of British India. Britain then only favored some of the native groups in Burma, and introduced many of their own traditions and customs. This harshly weakened the Burmese culture.</p>
     <p>Aung San was a young law student in Burma, who in 1836 left his classes to be more involved with politics. He was against the British, and their imperialist mentality. He joined the Dobama Asiayone (Our Burma Union) as a general secretary, and helped to organize strikes across the country.</p>
