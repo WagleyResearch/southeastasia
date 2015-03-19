1bc67ca0ed31cb130bc303482905b8c7cd9b7bcd
root <root@localhost.localdomain>
2015-03-19 01:02:59 -0400

    Added more optimization and a fixed header; still need work on header scaling

--- burma.php
+++ burma.php
@@ -4,7 +4,7 @@
         <img class="medium_image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Location_Burma_%28Myanmar%29_ASEAN.svg/1200px-Location_Burma_%28Myanmar%29_ASEAN.svg.png" alt="Location of &nbsp;Burma&nbsp;&nbsp;(green)in ASEAN&nbsp;&nbsp;(dark grey)&nbsp; –&nbsp; [Legend]">
     </div>
     A &ldquo;series of monarchs&rdquo; tried to take control of Burma. In the 1800s, the country had become very unstable, and Britain took advantage of this by trying to take over the region. After three wars over 60 years, Britain finally took control of the country, and annexed it to be a part of British India. Britain then only favored some of the native groups in Burma, and introduced many of their own traditions and customs. This harshly weakened the Burmese culture.
-    <div>
+    <div class="sub_content">
     <div class="image">
         <img class="small_image" src="http://www.aungsan.com/images/AungSan1gs.jpg">
     </div>
--- se.php
+++ se.php
@@ -1,5 +1,8 @@
-<div class="header">
-<h1>Southeast Asia</h1>
-How imperialism effected different parts of Southeast Asia<br>
-<i>By Ben Wagley, Jacob Hall, and Kennan Oizumi</i>
+<div class="header main_header">
+    <div class="header_fixed">
+        <h1>Southeast Asia</h1>
+             How imperialism effected different parts of Southeast Asia<br>
+        <i>By Ben Wagley, Jacob Hall, and Kennan Oizumi</i>
+    </div>
+    <img class="main_content_img" style="margin-top: 15%;" src="images/se.png"/>
 </div>
