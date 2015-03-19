fe8ad28bc0d918366fcc1cf584dfb5ec1f8fd431
totallyuneekname <student@HBW-STU-C17N327GG083.local>
2015-03-18 23:02:22 -0400

    Some things.

--- se.php
+++ se.php
@@ -1,4 +1,5 @@
-<div>
-<h1> Southeast Asia </h1>
-
+<div class="header">
+<h1>Southeast Asia</h1>
+How imperialism effected different parts of Southeast Asia<br>
+<i>By Ben Wagley, Jacob Hall, and Kennan Oizumi</i>
 </div>
