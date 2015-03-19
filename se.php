b5f12d71048a4ddaeda6d97762a8fcc0a365d70a
root <root@localhost.localdomain>
2015-03-19 09:24:39 -0400

    Fixed Margin Top Stuff

--- se.php
+++ se.php
@@ -1,8 +1,10 @@
 <div class="header main_header">
     <div class="header_fixed">
         <h1>Southeast Asia</h1>
-             How imperialism effected different parts of Southeast Asia<br>
+    </div>
+    <div class="header_main_content">
+        How imperialism effected different parts of Southeast Asia<br>
         <i>By Ben Wagley, Jacob Hall, and Kennan Oizumi</i>
     </div>
-    <img class="main_content_img" style="margin-top: 15%;" src="images/se.png"/>
+    <img class="main_content_img" src="images/se.png"/>
 </div>
