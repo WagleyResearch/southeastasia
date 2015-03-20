b9c838b2e8537aee13633fd166f5726c68138b70
totallyuneekname <student@HBW-STU-C17N327GG083.local>
2015-03-19 21:11:57 -0400

    new image css
    Tried to fix images. They actually float and stuff now. No, they are
    not perfect, but they are better and should fit all screens. They do
    not have a minimum pixel value, which we would want to set for very
    small screens such as iPhones. I also messed with the sub_content CSS,
    and think it turned out pretty well. What do you think?
    
    Oh yeah, and I uploaded those stupid images to the /Images directory.
    
    …and it’s affected, not effected. Someday I’ll get that right the first
    time :P

--- index.php
+++ index.php
@@ -6,8 +6,9 @@ error_reporting(E_ALL);
 <html>
     <head>
         <title>An overview of imperialism in Southeast Asia</title>
-        <link rel="stylesheet" media="screen and (max-width: 900px)" type="text/css" href="small.css">
-        <link rel="stylesheet" media="screen and (min-width: 901px)" type="text/css" href="main.css">
+        <!-- <link rel="stylesheet" media="screen and (max-width: 900px)" type="text/css" href="small.css"> -->
+        <!-- <link rel="stylesheet" media="screen and (min-width: 901px)" type="text/css" href="main.css"> -->
+        <link rel="stylesheet" type="text/css" href="new.css">
     </head>
     <body>
         <?php
--- se.php
+++ se.php
@@ -3,7 +3,7 @@
         <h1>Southeast Asia</h1>
     </div>
     <div class="header_main_content">
-        How imperialism effected different parts of Southeast Asia<br>
+        How imperialism affected different parts of Southeast Asia<br>
         <i>By Ben Wagley, Jacob Hall, and Kennan Oizumi</i>
     </div>
     <img class="main_content_img" src="images/se.png"/>
