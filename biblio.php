a6e02f4f6c9f0dafb2dce57a60af2fbcfdd813e5
totallyuneekname <student@HBW-STU-C17N327GG083.local>
2015-03-20 19:15:59 -0400

    some more minor changes
    not really in the mood to size things for mobile BUT things are getting
    better

--- biblio.php
+++ biblio.php
@@ -1,8 +1,10 @@
 <div class="main_content">
     <h2>Bibliography</h2>
     <ol style="line-height: 150%;">
-        <li>http://cfob.org/HistoryofBurma/historyOfBurma.shtml</li>
-        <li>http://en.wikipedia.org/wiki/Aung_San</li>
-        <li>To be continued...</li>
+        <li>haha I'm so lazy</li>
+        <li>I'm replacing these</li>
+        <li>with shorter lines</li>
+        <li>so it fits better on mobile</li>
+        <li>for now...</li>
     </ol>
 </div>
