af5a9697a018c518d0eb1127e80cd4296a1568ce
totallyuneekname <student@HBW-STU-C17N327GG083.local>
2015-03-20 12:07:52 -0400

    kinda broken but really cool
    yeah yeah I’ll fix it up

--- se.php
+++ se.php
@@ -6,5 +6,4 @@
         How imperialism affected different parts of Southeast Asia<br>
         <i>By Ben Wagley, Jacob Hall, and Kennan Oizumi</i>
     </div>
-    <img class="main_content_img" src="images/se.png"/>
 </div>
