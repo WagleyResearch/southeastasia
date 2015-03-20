3546c16ec699a1652d39157cdac42b23ae4b96e4
totallyuneekname <student@HBW-STU-C17N327GG083.local>
2015-03-19 21:21:34 -0400

    whoops. lowercase i

--- burma.php
+++ burma.php
@@ -1,13 +1,13 @@
 <div class="main_content">
     <h2>Burma</h2>
     <div class="rightimage" style="width: 30%;">
-        <img src="/Images/burma.png" alt="Location of &nbsp;Burma&nbsp;&nbsp;(green)in ASEAN&nbsp;&nbsp;(dark grey)&nbsp; â€“&nbsp; [Legend]">
+        <img src="/images/burma.png" alt="Location of &nbsp;Burma&nbsp;&nbsp;(green)in ASEAN&nbsp;&nbsp;(dark grey)&nbsp; â€“&nbsp; [Legend]">
     </div>
     <p>A &ldquo;series of monarchs&rdquo; tried to take control of Burma. In the 1800s, the country had become very unstable, and Britain took advantage of this by trying to take over the region. After three wars over 60 years, Britain finally took control of the country, and annexed it to be a part of British India. Britain then only favored some of the native groups in Burma, and introduced many of their own traditions and customs. This harshly weakened the Burmese culture.</p>
     <p>Another example of a paragraph in... Maecenas eu arcu a justo varius convallis. Nunc a purus pulvinar, tincidunt turpis at, lacinia nisl. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras neque nibh, consequat sit amet turpis vestibulum, semper hendrerit nisi. Nam quis turpis eu enim venenatis dictum. Morbi ut eros dignissim, auctor nisl at, hendrerit lacus. Ut mi magna, vestibulum ac purus et, dapibus malesuada purus. Sed varius pellentesque orci, in pulvinar quam pretium non. In eu lectus eu ipsum volutpat eleifend. Pellentesque eros felis, aliquam sit amet nibh vitae, ultricies porta est. Sed eget finibus lectus, hendrerit sollicitudin arcu. Aenean a auctor orci.</p>
     <div class="sub_content">
         <div class="leftimage" style="max-width: 15%;">
-            <img src="/Images/aungsan.jpg" alt="Picture of Aung San">
+            <img src="/images/aungsan.jpg" alt="Picture of Aung San">
         </div>
     <p>Aung San was a young law student in Burma, who in 1836 left his classes to be more involved with politics. He was against the British, and their imperialist mentality. He joined the Dobama Asiayone (Our Burma Union) as a general secretary, and helped to organize strikes across the country.<br><strong>This is pretty much just a test to show Ben how I think subcontent should work. Of course we can change the border and stuff, but this is scalable, still has margins, and catches the eye.</strong></p>
     </div>
