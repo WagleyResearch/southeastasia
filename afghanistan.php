ee9120edf48937583fd9f56cd2025c1f1df8b077
totallyuneekname <student@HBW-STU-C17N327GG083.local>
2015-03-21 13:46:25 -0400

    Started to add information of Afghanistan
    We need to add more content to our site!

--- afghanistan.php
+++ afghanistan.php
@@ -1,4 +1,4 @@
 <div class="main_content">
     <h2>British in Afghanistan</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p>One of Britain's major exports from India was opium, and by 1770 they had a manopoly on the product, and started to also grow it in Afghanistan. In 1839, Britain send 12,000 troops to the country, to remove the current king, Dost Mohammad, from power because they suspected him of being too friendly with Russia, which may have endangered their drug trade.</p>
 </div>
