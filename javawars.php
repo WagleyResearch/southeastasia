6240a392433c4fe13dc55ac19afcc0bff8e25c1e
Ben Wagley <benwagley@ben.fedora>
2015-03-23 13:05:59 -0400

    Tdsjkglf

--- javawars.php
+++ javawars.php
@@ -2,7 +2,7 @@
     <h2>Javanese Wars</h2>
     <p>In the 1800s, Java fought in a series of wars for independence, most notably the Java War (1741-43) and the Invasion of Java, in 1811. The former was against the Dutch, who in 1741 led an attack on Java to try to gain control of the region. The war lasted for three years, and both sides suffered many losses.</p>
   <div class="sub_content">
-    <img class="small_image" src="images/Diponegoro.jpg"/>
+    <img class="leftimage" style"max-width: 15%" src="images/Diponegoro.jpg"/>
       Prince Diponegoro, also know as Dipanegara, was a muslim prince from Java, that disliked the Dutch colonial rule of Java. One of his early conflicts with the Dutch, was when they passed over him when selecting the new Sultan, in favor of his brother, who was more favorable to the Dutch. Between local farmers becoming angered by the rise in taxes and the crop failures, and Javanese Nobles that wanted their right to lease land back, Diponegoro had a solid supporting base. He convinced the locals that he had indigenous rights to lead them, and declared a Holy War. Initially the Javanese were winning the fight, as the Dutch did not know how to combat their Guerilla tactics. However, once the Dutch really committed themselves to the war, and made Diponegoro take a more defensive position, he lost. Many of his men lost initiative, and deserted. The Dutch then captured him after inviting him to negotiation that were to be under a flag of truce.  They then exiled him to Makassar, a different island in the Dutch East Indies. 
   </p>
   </div>
