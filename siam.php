fba3a6abd5a6ab09e28f9fd919e0bd208453d3c5
totallyuneekname <student@HBW-STU-C17N327GG083.local>
2015-03-22 23:28:05 -0400

    small fix for the two sub content divs together that I forgot to commit

--- siam.php
+++ siam.php
@@ -4,7 +4,7 @@
         <img src="/images/siam.png" alt="Location of &nbsp;Siam&nbsp;&nbsp;(green)in ASEAN&nbsp;&nbsp;(dark grey)&nbsp; â€“&nbsp; [Legend]">
     </div>
     <p>Lay between british controlled Burma and French controlled Indo-China. Both countries tried to prevent each other to gain control over Saim. King Mongkut and his son Chulalongkorn tried to stay a neutral zone between the two powers. To do this, they developed schools, reformed the legal system, and reorganized the government. They also built telegraph systems, and railroads and also abolished slavery.</p>
-    <div class="sub_content">
+    <div class="sub_content" style="margin-bottom: 3%;">
         <div class="leftimage" style="max-width: 15%;">
             <img src="/images/kingmongkut.jpg" alt="Picture of Aung San">
         </div>
