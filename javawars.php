f52fd71332221bce60fa404a1a823da5d8009d24
Ben Wagley <benwagley@ben.fedora>
2015-03-23 16:39:20 -0400

    Added Amankurat

--- javawars.php
+++ javawars.php
@@ -7,5 +7,8 @@
     </div>
       Prince Diponegoro, also know as Dipanegara, was a muslim prince from Java, that disliked the Dutch colonial rule of Java. One of his early conflicts with the Dutch, was when they passed over him when selecting the new Sultan, in favor of his brother, who was more favorable to the Dutch. Between local farmers becoming angered by the rise in taxes and the crop failures, and Javanese Nobles that wanted their right to lease land back, Diponegoro had a solid supporting base. He convinced the locals that he had indigenous rights to lead them, and declared a Holy War. Initially the Javanese were winning the fight, as the Dutch did not know how to combat their Guerilla tactics. However, once the Dutch really committed themselves to the war, and made Diponegoro take a more defensive position, he lost. Many of his men lost initiative, and deserted. The Dutch then captured him after inviting him to negotiation that were to be under a flag of truce.  They then exiled him to Makassar, a different island in the Dutch East Indies. 
   </p>
+  </div></p>
+  <div class="sub_content">
+      King Amankurat II was the sultan of the Javanese Mataram people. However, he was quite helpless.  He began ruling with a very bad economy and a small army. his only option was to side with the Dutch.  The Dutch then fought to keep him in power, however, over time he would help people hide from the Dutch. Over time the Dutch distrusted him. After he died, his son became Sultan, but was quickly replaced as the Dutch disliked him.
   </div>
 </div>
