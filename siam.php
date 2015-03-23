fa0469fc396511a982dd55a1b06a9674f06020e4
totallyuneekname <student@HBW-STU-C17N327GG083.local>
2015-03-22 21:45:01 -0400

    ADDED CONTENT and some other things I guess

--- siam.php
+++ siam.php
@@ -3,5 +3,17 @@
     <div class="rightimage" style="max-width: 30%;">
         <img src="/images/siam.png" alt="Location of &nbsp;Siam&nbsp;&nbsp;(green)in ASEAN&nbsp;&nbsp;(dark grey)&nbsp; â€“&nbsp; [Legend]">
     </div>
-    <p>Lay between british controlled Burma and French controlled Indo-China. Both countries tried to prevent each other to gain control over Saim. King Mongkut and his son Chulalongkorn tried to stay a neutral zone between the two powers. To do this, they developed schools, reformed the legal system, and reorganized the government. They also built telegraph systems, and railroads and also abolished slavery.  </p>
+    <p>Lay between british controlled Burma and French controlled Indo-China. Both countries tried to prevent each other to gain control over Saim. King Mongkut and his son Chulalongkorn tried to stay a neutral zone between the two powers. To do this, they developed schools, reformed the legal system, and reorganized the government. They also built telegraph systems, and railroads and also abolished slavery.</p>
+    <div class="sub_content">
+        <div class="leftimage" style="max-width: 15%;">
+            <img src="/images/kingmongkut.jpg" alt="Picture of Aung San">
+        </div>
+        <p><strong>King mongkut</strong> (Rama IV) was one of the most revered Monarchs in Siam. The pressure of Westernization came for the first time in Siam. He accepted the innovations and initiated the advancements and the people nicknamed him the &ldquo;Father of science and technology&rdquo;</p>
+    </div>
+    <div class="sub_content">
+        <div class="leftimage" style="max-width: 15%;">
+            <img src="/images/chulalongkorn.jpeg" alt="Picture of Aung San">
+        </div>
+        <p><strong>Chulalongkorn</strong> (Rama V) he was one of the greatest monarchs in Siamese history. When Siam was threatened by western modernization, his policies and acts saved Siam from Colonization.</p>
+    </div>
 </div>
