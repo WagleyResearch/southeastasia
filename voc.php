6e1b22bb01133c72a93e14dc5813ca1eb45487ac
root <root@localhost.localdomain>
2015-03-23 14:03:19 -0400

    fixed pic voc

--- voc.php
+++ voc.php
@@ -1,16 +1,16 @@
 <div class="main_content">
     <h2>Dutch East India Company</h2>
-    <p> A company that held a 21 year monopoly on trade in the East Indies, from the country of the Netherlands. It was the first company to offer stock.  Set records in that it sent millions of Europeans to work in Asia on 4,785 ships.  The company had government like powers, with the power to mint currency, convict and execute people, negotiate treaties and found colonies. The Dutch East India Company or Vereenigde Oostindische Compagnie (VOC) was formed in response to the English East India Company, as the Dutch saw that they would loose out with their companies,  if they were not granted similar powers. The first permanent trading post formed by the VOC was Banten, West Java.  This sparked the first of the Dutch spice trading posts.  The English East India Company attempted to establish trading ports in the east indies, however after ten english men were beheaded for conspiracy while attempting to make a deal with the Dutch about trading in the area, the brits pulled out most of their trading from the region. The VOC killed almost everyone on the Banda islands. </p>
     <div class="leftimage">
         <img src="images/Dutch_VOC_ships_in_Chittagong_or_Arakan.jpg">
     </div>
+    <p> A company that held a 21 year monopoly on trade in the East Indies, from the country of the Netherlands. It was the first company to offer stock.  Set records in that it sent millions of Europeans to work in Asia on 4,785 ships.  The company had government like powers, with the power to mint currency, convict and execute people, negotiate treaties and found colonies. The Dutch East India Company or Vereenigde Oostindische Compagnie (VOC) was formed in response to the English East India Company, as the Dutch saw that they would loose out with their companies,  if they were not granted similar powers. The first permanent trading post formed by the VOC was Banten, West Java.  This sparked the first of the Dutch spice trading posts.  The English East India Company attempted to establish trading ports in the east indies, however after ten english men were beheaded for conspiracy while attempting to make a deal with the Dutch about trading in the area, the brits pulled out most of their trading from the region. The VOC killed almost everyone on the Banda islands. </p>
     <p></p>
     <div class="sub_content">
         The area that the Dutch East India Company colonized for trading was the
     Dutch East Indies, It became called the Dutch East Indies when the Dutch East India
 Company was dismantled and the Dutch government took control of the region. In
 1949, after many years of protest, the Dutch government recognized it as a sovereign nation.  This region produced many cash crops that the Dutch East India Company could trade, such as coffee, pepper, nutmeg, cloves, tea, cotton, sugar, cinnamon, and ginger.
-    </div>
+    </div></p>
     <div class="sub_content">
         The Anglo dutch treaty of 1824 was a treaty that allowed the British to control
 singapore, on the terms that they would now conduct any trading or diplomacy with
