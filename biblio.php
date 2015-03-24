fa1f473c1eeb07664ca233f9a33f020b47e9f239
root <root@localhost.localdomain>
2015-03-24 11:02:48 -0400

    Added to bib

--- biblio.php
+++ biblio.php
@@ -5,6 +5,7 @@
         <li><a href="http://en.wikipedia.org/wiki/Aung_San">http://en.wikipedia.org/wiki/Aung_San</a></li>
         <li><a href="https://en.wikipedia.org/wiki/Java_War">https://en.wikipedia.org/wiki/Java_War</a></li>
         <li><a href="https://en.wikipedia.org/wiki/Invasion_of_Java_%281811%29">https://en.wikipedia.org/wiki/Invasion_of_Java_%281811%29</a></li>
+        <li><a href="http://en.wikipedia.org/wiki/Anglo-Burmese_wars">http://en.wikipedia.org/wiki/Anglo-Burmese_wars</a></li>
         <li><a href="https://en.wikipedia.org/wiki/First_Anglo-Afghan_War">https://en.wikipedia.org/wiki/First_Anglo-Afghan_War</a></li>
         <li><a href="http://www.globalresearch.ca/afghanistan-the-legacy-of-the-british-empire-a-brief-history/5327994">http://www.globalresearch.ca/afghanistan-the-legacy-of-the-british-empire-a-brief-history/5327994</a></li>
         <li><a href="http://historoda.com/2013/04/07/the-age-of-imperialism-imperialism-in-southeast-asia/">http://historoda.com/2013/04/07/the-age-of-imperialism-imperialism-in-southeast-asia/</a></li>
