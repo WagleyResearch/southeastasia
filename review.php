dff57db6cbfa6237879f1c8128e3ef8b4ee8d4c5
root <root@localhost.localdomain>
2015-03-24 14:25:27 -0400

    added q

--- review.php
+++ review.php
@@ -1,6 +1,21 @@
 <div class="main_content">
     <h2>Review</h2>
-    <ol>
-        <li>Over about how many years did the three wars Burma fought against Britain last?</li>
-    </ol>
+    Over about how many years did the three wars Burma fought against Britain last?</p>
+	Dutch East India Company:</p>
+   <p style="text-indent: 5em;">  T/F      It was the only East India Company</p>
+   <p style="text-indent: 5em;"> T/F      The First Company to Offer Stock</p>
+   <p style="text-indent: 5em;"> T/F      Had Sovereign Powers.</p>
+Dutch East Indies:</p>
+  <p style="text-indent: 5em;">  T/F    Now is Modern day Indonesia</p>
+   <p style="text-indent: 5em;"> T/F    Had a trade Monopoly with the British</p>
+   <p style="text-indent: 5em;"> T/F    Exported spices, cotton, and coffee, among other things.</p>
+Javanese Wars:</p>
+   <p style="text-indent: 5em;"> T/F    Wars with the Dutch for Javanese Independence. </p>
+   <p style="text-indent: 5em;"> T/F    Prince Diponegoro was one of the leaders.</p>
+   <p style="text-indent: 5em;"> T/F    Resulted in Heavy Casualties on both sides.</p>
+Singapore:</p>
+   <p style="text-indent: 5em;"> T/F      A Region Originally Controlled by the Dutch</p>
+   <p style="text-indent: 5em;"> T/F    The Island had not yet been colonized by the Dutch</p>
+   <p style="text-indent: 5em;"> T/F     Main Export was spices </p>
+  <p style="text-indent: 5em;">  T/F    The British secured trade with the Anglo-Dutch treating.</p>
 </div>
