c5e1c05364c4b96fdc13d7175a229e5998069a3e
totallyuneekname <student@HBW-STU-C17N327GG083.local>
2015-03-24 11:53:19 -0400

    lots of additions

--- afghanistan.php
+++ afghanistan.php
@@ -1,4 +1,8 @@
 <div class="main_content">
     <h2>British in Afghanistan</h2>
     <p>One of Britain's major exports from India was opium, and by 1770 they had a manopoly on the product, and started to also grow it in Afghanistan. In 1839, Britain send 12,000 troops to the country, to remove the current king, Dost Mohammad, from power because they suspected him of being too friendly with Russia, which may have endangered their drug trade.</p>
+    <div class="sub_content">
+        <h3>Anglo-Afghan War</h3>
+        <p>The Anglo-Afghan War was</p>
+    </div>
 </div>
