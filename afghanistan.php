ecaa808271ba7439899e217b9b5e17538fedc3ba
totallyuneekname <student@HBW-STU-C17N327GG083.local>
2015-03-24 13:54:36 -0400

    content

--- afghanistan.php
+++ afghanistan.php
@@ -3,6 +3,10 @@
     <p>One of Britain's major exports from India was opium, and by 1770 they had a manopoly on the product, and started to also grow it in Afghanistan. In 1839, Britain send 12,000 troops to the country, to remove the current king, Dost Mohammad, from power because they suspected him of being too friendly with Russia, which may have endangered their drug trade.</p>
     <div class="sub_content">
         <h3>Anglo-Afghan War</h3>
-        <p>The Anglo-Afghan War was</p>
+        <p>The Anglo-Afghan War was fought between the British East India Company and Afghanistan, starting in 1839 and lasting three years.</p>
+    </div>
+    <div class="sub_content">
+        <img class="leftimage" src="/images/dostmohammad.jpg" alt="Dost Mohammad Khan">
+        <p><strong>Dost Mohammad Khan</strong> was an Afghan leader during the First Afghan War, and helped lead the country in battle against the British and Indian forces.</p>
     </div>
 </div>
