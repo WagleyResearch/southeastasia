be4da0151f18f411126c2f3fd4ac2cfae6169bb5
totallyuneekname <student@HBW-STU-C17N327GG083.local>
2015-03-25 12:01:52 -0400

    Added images

--- malaysia.php
+++ malaysia.php
@@ -1,5 +1,8 @@
 <div class="main_content">
     <h2>Malaysia</h2>
+    <div class="rightimage" style="max-width: 30%;">
+        <img src="/images/malaysia.png" alt="Location of &nbsp;Malaysia&nbsp;&nbsp;(green)in ASEAN&nbsp;&nbsp;(dark grey)&nbsp; â€“&nbsp; [Legend]">
+    </div>
     <p>Malaysia was ruled by the british in several patterns and forms of administration, beginning in the 1860s. Britain brought profound changes economically and socially to Malaysia. There was resistance to british rule and some revolts while they were expanding. People who resisted Britain were called treacherous rebels but later were called nationalist heroes by the malaysians. Britain gained peace and security, and also developed a industrial structure. They also improved transportation, public health facilities, reduced tropical diseases, and developed schools. However, they also exploited Malaysia’s natural resources. Imperialism in Malaysia was bad, but it was good.
 </p>
 </div>
--- philippines.php
+++ philippines.php
@@ -1,4 +1,7 @@
 <div class="main_content">
     <h2>The Philippines</h2>
+    <div class="rightimage" style="max-width: 30%;">
+        <img src="/images/philippines.png" alt="Location of &nbsp;The Philippines&nbsp;&nbsp;(green)in ASEAN&nbsp;&nbsp;(dark grey)&nbsp; â€“&nbsp; [Legend]">
+    </div>
     <p>The Philippines, Guam, and Puerto Rico were acquired by Spain. They tried to educate them and christianize the Filipinos but they resisted the Americans. Emilio Aguinaldo, the leader of Filipino nationalists demanded the United States grant them freedom from Spain in the event that they win the spanish American war. The United states won and promised the Philippines self rule. The U.S. built roads, railroads and hospitals to achieve this self rule for the Philippines. Led to food shortages because less crops were made.</p>
 </div>
